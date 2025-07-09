1f80ea0327296fa9d24c64426ebd42c122d99125
DevinRafa <devinrafa7@gmail.com>
2025-07-09 21:57:07 +0700

    user_name input

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
+user_name = input("masukan nama anda: ")
+
 teks_git_remote = "belajar meng-connection repo ke remote dari lokal ke github"
 list_command = "git remote <link https alamat repo github>", "git push -u origin master"
